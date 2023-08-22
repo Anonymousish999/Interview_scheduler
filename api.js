f57a133c0be9c06ad0907c9632a0fcb192972812
Anonymousish999 <guptarshu999@gmail.com>
2023-08-22 17:54:53 +0530

    auth done

--- api.js
+++ api.js
@@ -138,7 +138,8 @@ async function smartFunction(req,res){
 
 async function uploadCandidates(req,res){
     try{
-        await Candidate.upload();
+    
+        await Candidate.upload({candidates : req.body});
 
         res.json("success");
     }
